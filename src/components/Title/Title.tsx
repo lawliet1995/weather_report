5b684ed42a88a352cb741e7798b75c7e28cdc4bf
MSI\chengyaoxu <lawliet.tertium@gmail.com>
2021-10-05 15:51:02 +0800

    Add svg

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Edit from '../Edit/Edit';
 import styled from 'styled-components';
+import { ReactComponent as SettingIcon } from '../../assets/setting.svg';
 
 const TitleText = styled.div`
     font-family: fangsong;
@@ -8,14 +9,24 @@ const TitleText = styled.div`
     font-weight: 900;
     text-align: center;
     padding: 20px;
-`
+`;
+
+const Setting = styled(SettingIcon)`
+    float: right;
+    right: 25px;
+    width: 30px;
+    height: 44px;
+    cursor: pointer;
+    position: absolute;
+`;
 
 class Title extends React.Component {
   render() {
     return (
         <>
             <TitleText>
-                {this.props.children}            
+                {this.props.children}
+                <Setting />       
             </TitleText>
             <Edit show = {false}/>
         </>
