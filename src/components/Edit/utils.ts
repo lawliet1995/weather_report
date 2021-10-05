0655da81ef66ca7adae027761b5d1612c4a9a586
MSI\chengyaoxu <lawliet.tertium@gmail.com>
2021-10-05 22:09:33 +0800

    [feature] Add all cities

--- src/components/Edit/utils.ts
+++ src/components/Edit/utils.ts
@@ -99,7 +99,28 @@ export const availableLocations = [
       locationName: '板橋',
       sunriseCityName: '新北市',
     },
-  ];
+
+// 臺北市
+// 新北市
+// 桃園市
+// 臺中市
+// 臺南市
+// 高雄市
+// 基隆市
+// 新竹市
+// 嘉義市	
+// 宜蘭縣
+// 新竹縣
+// 苗栗縣	
+// 彰化縣
+// 南投縣
+// 雲林縣	
+// 嘉義縣
+// 屏東縣
+// 花蓮縣	
+// 臺東縣
+// 澎湖縣
+];
   
   export const findLocation = (cityName: string) => {
     return availableLocations.find((location) => location.cityName === cityName);
